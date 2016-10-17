310148c848e05d31a134222fa07f47b72c2f3841
Dion <Dion.Fulwood@gmail.com>
2016-10-17 00:40:40 -0700

    Creates books reducer with random books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+  { title: "My Bondage and My Freedom" },
+  { title: "Giovanni's Room" },
+  { title: "The Coldest Winter Ever" },
+  { title: "The Giving Tree" },
+  { title: "Scary Stories To Tell In The Dark"},
+  ]
+}
